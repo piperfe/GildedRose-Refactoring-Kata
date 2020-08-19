ece7b6db3d6ad511b9b14b2ea4778dc3ff112fe1
d0r019w <d0r019w@mac-d0r019s.local>
2020-08-19 17:20:57 -0400

    Add test for ageBrie to ensure that quality is not gt 50

--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -9,9 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class GildedRoseShould {
 
 
-    // unknown CHECK
-    // sulfuras
-
+    // AgeBrie
     @Test
     void when_item_name_is_AgedBrie_and_quality_is_lt_50_then_should_decrease_sellIn_and_increase_quality_in_1() {
         Item[] items = {new Item("Aged Brie", 10, 40)};
@@ -24,19 +22,33 @@ class GildedRoseShould {
     }
 
     @Test
-    void when_item_is_Backstage_and_sellIn_is_lt_6_and_quality_is_lt_48_then_should_increase_in_3_quality_and_decrease_sellIn_in_1() {
-        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47)};
+    void when_item_name_is_AgedBrie_and_quality_is_lt_50_and_sellin_is_lt_0_then_quality_should_increase_in_2_and_decrease_sellIn_in_1(){
+        Item [] items = {new Item("Aged Brie",-1, 48)};
+
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertEquals(4, gildedRose.items[0].sellIn);
         assertEquals(50, gildedRose.items[0].quality);
+
     }
 
     @Test
-    void when_item_is_AgedBrie_and_quality_is_lt_49_and_sellin_is_lt_0_then_quality_should_increase_in_1_and_decrease_sellIn_in_1(){
-        Item [] items = {new Item("Aged Brie",-1, 48)};
+    void when_item_name_is_AgedBrie_and_quality_is_eq_50_should_maintain_quality_and_decrease_sellIn_in_1(){
+        Item [] items = {new Item("Aged Brie",1, 50)};
+
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, gildedRose.items[0].quality);
+        assertEquals(0, gildedRose.items[0].sellIn);
+
+    }
+
+    @Test
+    void when_item_name_is_aged_brie_and_sellIn_is_lt_0_and_quality_is_eq_49_should_increase_quality_in_1_and_decrease_sell_in_in_1(){
+        Item [] items = {new Item("Aged Brie",-1, 49)};
 
         GildedRose gildedRose = new GildedRose(items);
 
@@ -45,6 +57,18 @@ class GildedRoseShould {
         assertEquals(50, gildedRose.items[0].quality);
         assertEquals(-2, gildedRose.items[0].sellIn);
 
+    }
+
+    // Backstage
+    @Test
+    void when_item_is_Backstage_and_sellIn_is_lt_6_and_quality_is_lt_48_then_should_increase_in_3_quality_and_decrease_sellIn_in_1() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(4, gildedRose.items[0].sellIn);
+        assertEquals(50, gildedRose.items[0].quality);
     }
 
     @Test
@@ -61,7 +85,7 @@ class GildedRoseShould {
 
 
 
-    // unknown CHECK
+    // Unknown
     @Test
     void when_item_name_is_unknown_and_quality_is_lt_0_and_sellin_is_gt_0_then_should_maintains_quality_and_decrease_sellin_1() {
         Item[] items = {new Item("Another Different Item", 10, -1)};
