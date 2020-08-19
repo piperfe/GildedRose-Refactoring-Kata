12675a23d7842c4733a48a5561a753b42ecc6940
piperfe <felipe.alfaro@walmart.com>
2020-08-19 18:20:44 -0400

    delete nested if's sellIn gt 0 (1 level)

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,46 +1,47 @@
 package com.gildedrose;
+
 class GildedRose {
-    Item[] items;
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private static final String AGED_BRIE = "Aged Brie";
+
+    Item[] items;
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
 
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+            if ((item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE)) && item.quality < 50) {
+
+                item.quality = item.quality + 1;
             }
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+            if (item.name.equals(BACKSTAGE) && item.sellIn < 11 && item.quality < 50) {
+                item.quality = item.quality + 1;
+
+            }
+            if (item.name.equals(BACKSTAGE) && item.sellIn < 6 && item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS) && item.quality > 0) {
+                item.quality = item.quality - 1;
+
+            }
+
+            if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
             if (item.sellIn < 0) {
                 if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!item.name.equals(BACKSTAGE)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
--- Java/src/test/java/com/gildedrose/GildedRoseShould.java
+++ Java/src/test/java/com/gildedrose/GildedRoseShould.java
@@ -1,10 +1,8 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseShould {
 
